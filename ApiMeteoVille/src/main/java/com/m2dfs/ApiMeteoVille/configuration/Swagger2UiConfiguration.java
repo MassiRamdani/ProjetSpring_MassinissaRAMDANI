db578d3c1eb9de57e25221aa1ca49c732b7b86d7
massinissa06 <massinfoo@gmail.com>
2021-11-19 16:31:48 +0100

    Configure swagger

--- ApiMeteoVille/src/main/java/com/m2dfs/ApiMeteoVille/configuration/Swagger2UiConfiguration.java
+++ ApiMeteoVille/src/main/java/com/m2dfs/ApiMeteoVille/configuration/Swagger2UiConfiguration.java
@@ -1,9 +1,9 @@
 package com.m2dfs.ApiMeteoVille.configuration;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -14,11 +14,12 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class Swagger2UiConfiguration extends WebMvcConfigurerAdapter {
+
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis((RequestHandlerSelectors.any()))
                 .paths(PathSelectors.any())
                 .build()
                 ;
@@ -29,6 +30,4 @@ public class Swagger2UiConfiguration extends WebMvcConfigurerAdapter {
         registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
         registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
-
-
 }
