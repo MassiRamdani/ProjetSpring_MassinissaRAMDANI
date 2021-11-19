12a4b4db9313f6f063bc5c412fc5684eb5df7cde
massinissa06 <massinfoo@gmail.com>
2021-11-19 09:00:38 +0100

    create apiMicroService

---
+++ ApiRestMeteo/src/main/java/com/m2dfs/ApiRestMeteo/ApiRestMeteoApplication.java
@@ -0,0 +1,13 @@
+package com.m2dfs.ApiRestMeteo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestMeteoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestMeteoApplication.class, args);
+	}
+
+}
---
+++ ApiRestMeteo/src/test/java/com/m2dfs/ApiRestMeteo/ApiRestMeteoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.m2dfs.ApiRestMeteo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestMeteoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
